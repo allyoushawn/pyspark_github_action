7119ea5f87c6fbc3d5d83325a0b63fe5c88ef752
Yu-Hsuan Wang <k43272001@gmail.com>
2023-06-24 22:10:19 -0700

    update

--- main.py
+++ main.py
@@ -2,7 +2,23 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, LongType, StringType
 
+def create_dataset():
+    data = [
+        (1001, "[0.1,0.1]"),
+        (1002, "[0.2,0.2]"),
+    ]
+
+    schema = StructType(
+        [
+            StructField("userNumber", LongType(), True),
+            StructField("vector", StringType(), True),
+        ]
+    )
+    dataset = spark.createDataFrame(data=data, schema=schema)
+    dataset.show()
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +27,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    spark = SparkSession.builder.appName("TestPySpark").enableHiveSupport().getOrCreate()
+    create_dataset()
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
